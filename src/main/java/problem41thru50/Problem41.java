3e6bfa3da57db384efd0d820eb3c7569fb65d613
leto <not_an_email@website.com>
2020-07-29 11:41:14 -0400

    Problem 41 solved.

--- src/main/java/problem41thru50/Problem41.java
+++ src/main/java/problem41thru50/Problem41.java
@@ -22,28 +22,48 @@ public class Problem41 {
 	}
 
 	private static long pandigitalPrime() {
-		long answer = approach1();
+//		long answer = approach1();
+		long answer = approach2();
 		return answer;
+	}
+
+	/*
+	 * Generate pandigitals, keep track of the largest prime.
+	 * 
+	 * Took about 2 minutes to print all the pandigitals (i.e. less than or equal to 987654321)
+	 */
+	static long approach2() {
+		long largest = -1;
+		List<Long> pandigitals = new LinkedList<Long>();
+		for (long i = 1; i <= 987654321; i++) {
+			if (Pandigital.isPandigital(i)) {
+				pandigitals.add(i);
+			}
+		}
+		System.out.println("Problem41>> sorting... ");
+		Collections.sort(pandigitals, Collections.reverseOrder());
+		System.out.println("Problem41>> sorted. ");
+		for (Long l : pandigitals) {
+			if (Prime.isPrime(l)) {
+				System.out.println("Problem41>> found " + l);
+				largest = l;
+				break;
+			} else {
+				System.out.println(String.format("Problem41>> %s is not prime.", l));
+			}
+		}
+		
+		return largest;
 	}
 
 	/*
 	 * While generating all primes <= 987654321,
 	 * collect those which are pandigital.
 	 * Find the largest panditigal in that list.
+	 * 
+	 * This ran for more than 24 hours.
 	 */
 	public static long approach1() {
-		/*
-		 * List pandigitalPrimes = new List();
-		 * int prime = 1;
-		 * while (prime < 987654321) {
-		 *   prime = Prime.nextPrime(prime);
-		 *   if (Pandigital.isPandigital(prime)) {
-		 *     pandigitalPrimes.add(prime);
-		 *   }
-		 * }
-		 * 
-		 * return Collections.max(pandigitalPrimes);
-		 */
 		List<Long> pandigitalPrimes = new LinkedList<Long>();
 		long prime = 1;
 		while (prime < 987654321) {
